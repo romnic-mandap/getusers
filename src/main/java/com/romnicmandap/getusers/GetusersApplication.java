cdb040dd66a66ba3bdd13eafa35a795395cee3f6
Romar Nicholas G. Mandap <romnic.mandap@gmail.com>
2026-04-09 19:14:58 +0800

    init project

---
+++ src/main/java/com/romnicmandap/getusers/GetusersApplication.java
@@ -0,0 +1,13 @@
+package com.romnicmandap.getusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetusersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetusersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/romnicmandap/getusers/GetusersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romnicmandap.getusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetusersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
