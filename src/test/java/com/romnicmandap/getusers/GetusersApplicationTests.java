bb5e71feeb2039a82cbe69d524eb3eb56a5dfd7f
Romar Nicholas G. Mandap <romnic.mandap@gmail.com>
2026-04-09 22:30:38 +0800

    add test

--- src/test/java/com/romnicmandap/getusers/GetusersApplicationTests.java
+++ src/test/java/com/romnicmandap/getusers/GetusersApplicationTests.java
@@ -1,13 +1,21 @@
 package com.romnicmandap.getusers;
 
+import com.romnicmandap.getusers.enricheduser.EnrichedUserController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class GetusersApplicationTests {
 
+	@Autowired
+	private EnrichedUserController controller;
+
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
